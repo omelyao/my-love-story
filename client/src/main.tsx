22577ce738fb6d7bdf04e79c231d94ed7328b13f
Kolya <ryt9992@mail.ru>
2026-08-18 23:48:45 +0500

    3423

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,7 @@ import '../app/styles/global.css'
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
       <Provider store={store}>
-         <BrowserRouter>
+         <BrowserRouter basename="/my-love-story">
             <App />
          </BrowserRouter>
       </Provider>
